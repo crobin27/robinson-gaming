755cba6b579804610a29e2788fea18f87d24738f
crobin27 <crobin27@calpoly.edu>
2024-04-26 22:14:53 -0700

    ready for integration testing/transferring of services

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,14 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
-} // Example customization: specify a site base URL, build options, integrations, etc.
-// integrations: [],
-);
+  integrations: [
+    tailwind(),
+    sitemap({
+      // Specify the site's base URL. Necessary for generating the correct links in the sitemap
+      site: 'https://www.robinsongaming.com',
+    })
+  ]
+});
