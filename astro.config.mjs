04de629ee68294167aa98b93e9644e24c297a90d
crobin27 <crobin27@calpoly.edu>
2024-10-30 23:01:41 -0700

    Hero mostly working, need to finalize css grouping and sizing

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,22 +1,21 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-import sitemap from '@astrojs/sitemap';
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  publicDir: './public', // default is 'public'
+  publicDir: "./public", // default is 'public'
   integrations: [
     tailwind(),
     sitemap({
       // Specify the site's base URL. Necessary for generating the correct links in the sitemap
-      site: 'https://www.robinsongaming.com',
-    })
+      site: "https://www.robinsongaming.com",
+    }),
   ],
 
   vite: {
     ssr: {
       external: ["svgo"],
-    }
-  }
-
+    },
+  },
 });
