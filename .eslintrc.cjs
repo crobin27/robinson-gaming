9b4136e655647fd3032e0ddd918d64770372a37a
crobin27 <crobin27@calpoly.edu>
2025-11-02 09:16:14 -0800

    fix: deploy gh actions issue

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,16 +1,16 @@
 module.exports = {
   extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:astro/recommended',
-    'plugin:react/recommended',
-    'plugin:react-hooks/recommended',
+    "eslint:recommended",
+    "@typescript-eslint/recommended",
+    "plugin:astro/recommended",
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
   ],
-  parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint', 'astro', 'react', 'react-hooks'],
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint", "astro", "react", "react-hooks"],
   parserOptions: {
     ecmaVersion: 2020,
-    sourceType: 'module',
+    sourceType: "module",
     ecmaFeatures: {
       jsx: true,
     },
@@ -21,40 +21,34 @@ module.exports = {
     node: true,
   },
   rules: {
-    'react/react-in-jsx-scope': 'off',
-    'react/prop-types': 'off',
-    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    "react/react-in-jsx-scope": "off",
+    "react/prop-types": "off",
+    "@typescript-eslint/no-unused-vars": ["error", { argsIgnorePattern: "^_" }],
   },
   overrides: [
     {
-      files: ['*.astro'],
-      parser: 'astro-eslint-parser',
+      files: ["*.astro"],
+      parser: "astro-eslint-parser",
       parserOptions: {
-        parser: '@typescript-eslint/parser',
-        extraFileExtensions: ['.astro'],
+        parser: "@typescript-eslint/parser",
+        extraFileExtensions: [".astro"],
       },
       rules: {
-        'astro/no-set-html-directive': 'error',
-        'astro/no-unused-css-selector': 'error',
-        // Disable React rules for Astro files
-        'react/no-unknown-property': 'off',
-        'react/jsx-no-target-blank': 'off',
-        'react/jsx-key': 'off',
-        'react/no-unescaped-entities': 'off',
-        'react/jsx-no-duplicate-props': 'off',
+        "astro/no-set-html-directive": "error",
+        "astro/no-unused-css-selector": "error",
       },
     },
     {
-      files: ['*.tsx'],
+      files: ["*.tsx"],
       rules: {
-        'react-hooks/rules-of-hooks': 'error',
-        'react-hooks/exhaustive-deps': 'warn',
+        "react-hooks/rules-of-hooks": "error",
+        "react-hooks/exhaustive-deps": "warn",
       },
     },
   ],
   settings: {
     react: {
-      version: 'detect',
+      version: "detect",
     },
   },
 };
